f51ac56ddb68c869b346cad4e3f4658f9d97601f
velafarq <vela.mrdjen@gmail.com>
2018-10-02 12:06:10 -0500

    Add min height to elements and remove text underline on non links"

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  // PORT: process.env.PORT || 8080,
-  API_BASE_URL: process.env.REACT_APP_API_BASE_URL || ""
+  PORT: process.env.PORT || 3000,
+  API_BASE_URL: process.env.REACT_APP_API_BASE_URL || "http://localhost:8080"
 };
